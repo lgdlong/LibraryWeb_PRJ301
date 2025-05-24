a0fb243d6738e0bc09584d7e2a041aa2d4e1ea0e
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-05-24 23:09:15 +0700

    chore(): update

---
+++ src/java/entity/User.java
@@ -0,0 +1,79 @@
+package entity;
+
+import enums.*;
+
+public class User {
+    private long id;
+    private String username;
+    private String password;
+    private String email;
+    private UserRole role;
+    private UserStatus userStatus;
+
+    // Default constructor
+    public User() {
+    }
+
+    // Constructor for creating a user
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = UserRole.USER; // Default role
+        this.userStatus = UserStatus.ACTIVE; // Default status
+    }
+
+    // Constructor for retrieving a user from the database
+    public User(long id, String username, String password, String email, UserRole role, UserStatus userStatus) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.userStatus = userStatus;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public UserStatus getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(UserStatus userStatus) {
+        this.userStatus = userStatus;
+    }
+}
